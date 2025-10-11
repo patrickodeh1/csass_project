1cb1a6f9aea8ff006aed7e2ae7be51603097fbe7
Patrick Odeh <vyltyan@gmail.com>
2025-10-11 14:19:00 +0000

    test

--- csass_project/settings.py
+++ csass_project/settings.py
@@ -12,6 +12,9 @@ IS_BUILDING = os.getenv('SECRET_KEY') == 'temp-build-key'
 
 # SECRET_KEY - use environment variable in production
 if IS_CLOUD_RUN:
+    print("🌩️  Running on Cloud Run:", IS_CLOUD_RUN)
+    print("🪣  Using bucket:", os.getenv("GS_BUCKET_NAME"))
+
     SECRET_KEY = os.getenv('SECRET_KEY')
 elif IS_BUILDING:
     SECRET_KEY = 'temp-build-key'
